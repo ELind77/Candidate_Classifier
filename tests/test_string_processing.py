9655f09f60a9ba0607841e549214eb2e10e9603f
Eric Lind <Eric.Lind.7@gmail.com>
2016-03-01 19:39:06 -0500

    Adds tests

---
+++ tests/test_string_processing.py
@@ -0,0 +1,27 @@
+import nose.tools as nosey
+from candidate_classifier.string_processing import *
+
+
+DOC1 = u"I'm starting to know what God felt like when he sat out " \
+       "there in the darkness, creating the world."
+
+DOC2 = u"And what did he feel like, Lloyd, my dear?"
+
+DOC3 = u"Very pleased he'd taken his Valium."
+
+DOCS = [DOC1, DOC2, DOC3]
+
+
+def test_string_processor_returns_string_for_string():
+    processor = TransformerABC()
+    nosey.assert_equal(DOC1, processor(DOC1))
+
+
+def test_string_processor_returns_list_for_string():
+    processor = TransformerABC(tokenizer=string.split)
+    nosey.assert_equal(DOC1.split(), processor(DOC1))
+
+
+def test_string_processor_works_as_a_generator():
+    processor = TransformerABC()
+    nosey.assert_equal(DOCS, [d for d in processor(DOCS)])
